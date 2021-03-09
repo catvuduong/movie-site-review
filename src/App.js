4908192313359f63a6956efa474198ba90674e26
catvu17 <duongcatvu17@gmail.com>
2021-03-09 19:51:47 +0700

    update movie-site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PageNotFound from "./pages/page-not-found";
+import { routeAdmin, routesHome } from "./routes";
+import HomeTemplate from './templates/home-template';
+import AdminTemplate from './templates/admin-template';
+import Admin from './pages/admin/admin';
+
+const showMenuHome = routes => {
+  if (routes && routes.length > 0) {
+    return routes.map((item, index) => {
+      return (
+        <HomeTemplate
+          key={index}
+          path={item.path}
+          exact={item.exact}
+          Component={item.component}
+        />
+      );
+    });
+  };
+};
+
+const showMenuAdmin = routes => {
+  if (routes && routes.length > 0) {
+    return routes.map((item, index) => {
+      return (
+        <AdminTemplate
+          key={index} path={item.path} exact={item.exact} Component={item.component}
+        ></AdminTemplate>
+      );
+    });
+  };
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* <Navbar /> */}
+        <Switch>
+          {showMenuHome(routesHome)}
+          {showMenuAdmin(routeAdmin)}
+          <Route path="/admin" component={Admin}></Route>
+          <Route path="" component={PageNotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/popper.js/dist/umd/popper.min.js';
+import './../node_modules/jquery/dist/jquery.min.js';
+import './../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/reducers/root-reducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App></App>
+  </Provider>,
   document.getElementById('root')
 );
 
